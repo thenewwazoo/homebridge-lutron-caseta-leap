d86c329174982e4e7e07d4c76bf3bd63ac2e593f
Brandon Matthews <bmatt@optimaltour.us>
2022-02-26 00:46:29 -0800

    Add support for UI-based SmartBridge pairing
    This change adds a custom plugin configuration UI that enables users to
    pair with bridges without having to install Python 3 or know their
    bridge's IP address.

--- src/BridgeManager.ts
+++ src/BridgeManager.ts
@@ -1,4 +1,7 @@
 import { SmartBridge } from 'lutron-leap';
+import { LutronCasetaLeap } from './platform';
+
+import * as util from 'util';
 
 export class BridgeManager {
     /* When restoring accessories from the cache, the mDNS-based bridge
@@ -15,8 +18,13 @@ export class BridgeManager {
     private pendingBridges: Map<string, Array<[(bridge: SmartBridge) => void, ReturnType<typeof setTimeout>]>> =
         new Map(); // whew, that's a gnarly spec.
 
+    constructor(private platform: LutronCasetaLeap) {}
+
     public getBridge(bridgeID: string): Promise<SmartBridge> {
+        this.platform.log.debug('got request for bridge', bridgeID);
+
         if (this.bridges.has(bridgeID)) {
+            this.platform.log.debug('had bridge', bridgeID);
             return Promise.resolve(this.bridges.get(bridgeID)!);
         } else {
             // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -30,8 +38,10 @@ export class BridgeManager {
             };
 
             const pendingTimeout = setTimeout(() => {
+                this.platform.log.error('pending map was', util.inspect(this.pendingBridges));
+                this.platform.log.error('constructed bridges', util.inspect(this.bridges));
                 rejectPending(new Error('Timed out waiting for bridge to appear'));
-            }, 5000);
+            }, 15000);
 
             const bridgePromise: Promise<SmartBridge> = new Promise(
                 (resolve: (bridge: SmartBridge) => void, reject) => {
@@ -50,8 +60,10 @@ export class BridgeManager {
     }
 
     public addBridge(bridge: SmartBridge) {
+        this.platform.log.debug('got request to add bridge', bridge.bridgeID);
         this.bridges.set(bridge.bridgeID, bridge);
         if (this.pendingBridges.has(bridge.bridgeID)) {
+            this.platform.log.debug('someone was awaiting bridge', bridge.bridgeID);
             for (const [p, t] of this.pendingBridges.get(bridge.bridgeID)!) {
                 clearTimeout(t);
                 p(bridge);
@@ -61,6 +73,7 @@ export class BridgeManager {
     }
 
     public hasBridge(bridgeID: string) {
+        this.platform.log.debug('yeah, I\'ve got', bridgeID, 'who wants to know?');
         return this.bridges.has(bridgeID);
     }
 }
